bf8c1169fbef04cc0f6c4909a6f895872794a37b
artdev-hash <kim.dev.pr@gmail.com>
2024-03-25 21:32:42 +0800

    deploy

---
+++ assets/posts_04.md.E1bMuXy6.lean.js
@@ -0,0 +1,14 @@
+import{_ as a,c as t,o as r,k as e,a as o}from"./chunks/framework.bbhau2DI.js";const H=JSON.parse('{"title":"Computer Hardware","description":"A definition of Computer Hardware","frontmatter":{"title":"Computer Hardware","description":"A definition of Computer Hardware","date":"2024-02-18T00:00:00.000Z","tags":["Personal Computer"]},"headers":[],"relativePath":"posts/04.md","filePath":"posts/04.md"}'),n={name:"posts/04.md"},s=e("h2",{id:"what-is-a-computer-hardware",tabindex:"-1"},[o("What is a Computer Hardware ? "),e("a",{class:"header-anchor",href:"#what-is-a-computer-hardware","aria-label":'Permalink to "What is a Computer Hardware ?"'},"​")],-1),i=e("p",null,"Computer hardware comprises all the physical elements of a PC. Hardware refers to the external and internal devices and equipment that enable you to perform major functions such as input, output, storage, communication, processing, and more.",-1),d=e("p",null,"These components include:",-1),c=e("pre",null,[e("code",null,`-Monitor
+-Keyboard
+-Central Processing Unit (CPU)
+-Motherboard
+-Hard Disk Drive (HDD)
+-Optical drive (CD & DVD)
+-Mouse
+-Random Access Memory (RAM)
+-Speakers
+-Graphics card
+-Sound card
+-Fans
+-Printer
+`)],-1),p=[s,i,d,c];function l(m,u,h,_,f,w){return r(),t("div",null,p)}const D=a(n,[["render",l]]);export{H as __pageData,D as default};
